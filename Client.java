10e65e276739ac25991d82b76cbf5dac42a054d0
tabita-emapohus <161648276+tabita-emapohus@users.noreply.github.com>
2024-06-19 12:43:21 +0300

    Create class Client.java

---
+++ Client.java
@@ -0,0 +1,17 @@
+
+public class Client {
+
+	private String nume;
+	private String prenume;
+	public Client(String nume, String prenume) {
+		this.nume = nume;
+		this.prenume = prenume;
+	}
+	public String getNume() {
+		return nume;
+	}
+	public String getPrenume() {
+		return prenume;
+	}
+	
+}

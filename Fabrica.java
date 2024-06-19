03d539028f98fddf6765323d0b10e28d3c937904
tabita-emapohus <161648276+tabita-emapohus@users.noreply.github.com>
2024-06-19 12:42:49 +0300

    Create class Fabrica.java

---
+++ Fabrica.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Fabrica extends Client{
+	private private ArrayList<Angajat> angajati;
+	
+	public Fabrica () {
+		angajati = new ArrayList<Angajat>();
+	}
+	public Fabrica(ArrayList<Angajat> angajati) {
+		this.angajati = angajati;
+
+}
+private private ArrayList<Client> clienti;
+	
+	public Fabrica () {
+		angajati = new ArrayList<Client>();
+	}
+	public Fabrica(ArrayList<Clinet> clineti) {
+		this.clineti = clienti;
+}
+	public void adaugareAngajati(){
+		
+	}
+	public void adaugareClienti(){
+		
+	}
+	public void afisareAngajati(){
+		
+	}
+	public void afisareContract(){
+		
+	}
+	public void afisareActeEmise(){
+	
+	}
+	
+}

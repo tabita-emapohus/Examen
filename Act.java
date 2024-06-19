3ece77405a5cd76c37d4f07eb7ed480d4e04cadd
tabita-emapohus <161648276+tabita-emapohus@users.noreply.github.com>
2024-06-19 12:43:55 +0300

    Create class Act.java

---
+++ Act.java
@@ -0,0 +1,17 @@
+
+import java.util.HashMap;
+
+public class Act {
+
+	private private HashMap<Act> ID;
+	public Act () {
+		ID = new HashMap<Act>();
+	
+}
+	public HashMap<Act> getID() {
+		return ID;
+	}
+	public void FluturasSalariu() {
+		
+	}
+}
